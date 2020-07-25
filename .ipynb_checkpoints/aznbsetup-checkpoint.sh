9fe6de0423c567781a5b34224bc80671f125aee7
Dan Harrison <87bdharr@gmail.com>
2020-07-25 10:53:14 +0100

    first commit

---
+++ .ipynb_checkpoints/aznbsetup-checkpoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#pip
+pip install urllib3
+pip install spacy
+python3 -m spacy download en_core_web_sm
+pip install nltk
+pip install beautifulsoup4
+pip install textacy
+pip install feedparser
---
+++ aznbsetup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Activate environment
+source/home/nbcommon/anaconda3_510/bin/activate
+
+#pip
+pip install urllib3
+pip install spacy
+pip install nltk
+pip install beautifulsoup4
+pip install textacy
+pip install feedparser
